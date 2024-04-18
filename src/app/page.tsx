1f3c405ee77a01bb958b0f7d2a126f3879632427
Sohan <svaigankar2000@gmail.com>
2024-04-19 01:50:39 +0530

    schema changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,32 +4,24 @@ import { db } from "~/server/db";
 // update the page content when any change is made to the db
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/383a11fc-f7c1-49d3-96a2-ec628b2309c0-jtoka6.jpg",
-  "https://utfs.io/f/c1e54cfd-f85a-4715-9b49-b3b5c2395afd-8lmoxn.jpg",
-  "https://utfs.io/f/bb0acc60-ff47-410c-9bd4-8361052a8fd9-d0wbgw.jpg",
-  "https://utfs.io/f/a28ab6fd-eba7-47b3-ac3e-362c10311066-p1vz3k.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
-
 const HomePage = async () => {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts?.map((post) => <div key={post.id}>{post.name}</div>)}
-
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {images.map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
             <Image
               src={image.url}
-              alt={"img"}
+              alt={image.name}
               width={0}
               height={0}
               // sizes="100vw"
               style={{ width: "100%", height: "auto" }}
             />
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
