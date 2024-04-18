99b2e4fbb3dccfbcebfcf20d46fa87b7e1c3374a
Sohan <svaigankar2000@gmail.com>
2024-04-19 00:29:58 +0530

    build improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import Link from "next/link";
+import Image from "next/image";
+
+const mockUrls = [
+  "https://utfs.io/f/383a11fc-f7c1-49d3-96a2-ec628b2309c0-jtoka6.jpg",
+  "https://utfs.io/f/c1e54cfd-f85a-4715-9b49-b3b5c2395afd-8lmoxn.jpg",
+  "https://utfs.io/f/bb0acc60-ff47-410c-9bd4-8361052a8fd9-d0wbgw.jpg",
+  "https://utfs.io/f/a28ab6fd-eba7-47b3-ac3e-362c10311066-p1vz3k.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <div>
-      Gallery
-     </div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <Image
+              src={image.url}
+              alt={"img"}
+              width={0}
+              height={0}
+              // sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
