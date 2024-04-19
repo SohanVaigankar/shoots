4fb66896cb372a8fc69ec3c5160789d4a4b06d84
Sohan <svaigankar2000@gmail.com>
2024-04-20 02:31:59 +0530

    added authentication and image upload using upload thing

---
+++ src/app/_components/TopNav.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
+
+const TopNav = () => {
+  const router = useRouter();
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={() => {
+            router.refresh();
+          }}
+        />
+        <UserButton />
+      </SignedIn>
+    </nav>
+  );
+};
+
+export default TopNav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,42 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 
 // update the page content when any change is made to the db
 export const dynamic = "force-dynamic";
 
-const HomePage = async () => {
+const renderImages = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <Image
+            src={image.url}
+            alt={image.name}
+            width={0}
+            height={0}
+            // sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const HomePage = async () => {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <Image
-              src={image.url}
-              alt={image.name}
-              width={0}
-              height={0}
-              // sizes="100vw"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in to access images
+        </div>
+      </SignedOut>
+      <SignedIn>{renderImages()}</SignedIn>
     </main>
   );
 };
